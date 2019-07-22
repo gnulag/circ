442a95b8efd0c115daf3dca044042b406ea7f15e
Max Audron <audron@ejected.space>
2019-07-22 08:35:11 +0200

    add new variables to start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 CIRC_DEBUG="yes please" \
-CIRC_SERVER=irc.snoonet.org \
+CIRC_SERVER=localhost \
 CIRC_PORT=6667 \
 CIRC_SSL=false \
 CIRC_NICK=circ \
 CIRC_REALNAME=https://github.com/gnulag/circ/ \
 CIRC_IDENT=circ \
+CIRC_SASL_ENABLED=false \
+CIRC_AUTH_USER=circ \
+CIRC_AUTH_PASS=circ \
+CIRC_CHANNEL=#gnulag \
 ./build/circ
