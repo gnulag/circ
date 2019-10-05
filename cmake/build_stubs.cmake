c0fbe38e3aa575f894d4b82ea74033457669fd87
Max Audron <audron@cocaine.farm>
2019-10-05 17:52:45 +0200

    fix make not finding target for chibi-ffi
    it was looking in the SOURCE_DIR and not in BINARY_DIR

--- cmake/build_stubs.cmake
+++ cmake/build_stubs.cmake
@@ -1,14 +1,14 @@
-file(GLOB_RECURSE stubs RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/scheme_libs
-  ${CMAKE_CURRENT_SOURCE_DIR}/scheme_libs/*.stub)
+file(GLOB_RECURSE stubs RELATIVE ${CMAKE_CURRENT_BINARY_DIR}/scheme_libs
+  ${CMAKE_CURRENT_BINARY_DIR}/scheme_libs/*.stub)
 
-set(chibi-ffi ${CMAKE_CURRENT_SOURCE_DIR}/chibi-scheme/tools/chibi-ffi)
-set(chibi-genstatic ${CMAKE_CURRENT_SOURCE_DIR}/chibi-scheme/tools/chibi-genstatic)
+set(chibi-ffi ${CMAKE_CURRENT_BINARY_DIR}/chibi-scheme/tools/chibi-ffi)
+set(chibi-genstatic ${CMAKE_CURRENT_BINARY_DIR}/chibi-scheme/tools/chibi-genstatic)
 
 set(stuboutdir ${CMAKE_CURRENT_BINARY_DIR}/scheme_libs/)
 foreach(e ${stubs})
   get_filename_component(stubdir ${e} PATH)
   get_filename_component(basename ${e} NAME_WE)
-  set(stubfile ${CMAKE_CURRENT_SOURCE_DIR}/scheme_libs/${e})
+  set(stubfile ${CMAKE_CURRENT_BINARY_DIR}/scheme_libs/${e})
   set(stubdir ${stuboutdir}/${stubdir})
   set(stubout ${stubdir}/${basename}.c)
   set(libout ${stubdir}/${basename}.so)
@@ -16,11 +16,11 @@ foreach(e ${stubs})
   add_custom_command(OUTPUT ${stubout}
     COMMAND chibi-scheme ${chibi-ffi} ${stubfile} ${stubout}
     DEPENDS ${stubfile} ${chibi-ffi}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
   add_custom_command(OUTPUT ${libout}
     COMMAND cc -fPIC -shared ${stubout} -o ${libout}
     DEPENDS ${stubout}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
   list(APPEND stubouts ${stubout})
   list(APPEND libouts ${libout})
 endforeach()
